6e0609ed5ff72850e1089d0c6a0699292a4c41ad
AhsanTariqMajeed <ahsantariqmajeed@gmail.com>
2022-08-16 15:58:17 +0500

    Create Age converter.js

---
+++ Programs/Age converter.js
@@ -0,0 +1,18 @@
+//my age
+let myAge = 29;
+let earlyYears = 2
+
+earlyYears = earlyYears * 10.5;
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+let myAgeInDogYears = laterYears + earlyYears;
+console.log(laterYears);
+console.log(earlyYears);
+
+const myName = "Ahsan".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+
